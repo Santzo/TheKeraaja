b429edfcf560391845962bc4b5c528c0e32d5e2d
Santeri Hakoniemi <santeri.hakoniemi@gmail.com>
2020-01-15 14:37:00 +0200

    15.1.2020

--- Assets/Scripts/KaytavaManager.cs
+++ Assets/Scripts/KaytavaManager.cs
@@ -6,17 +6,20 @@ using UnityEngine;
 
 public class KaytavaManager : MonoBehaviour
 {
-    TextMeshProUGUI timer;
-    Transform indicator;
+    TextMeshProUGUI timer, seuraava, etaisyys;
+    Transform indicator, player;
     Transform[] kaytavat;
     float time = 0f;
     Vector3 indicatorOri;
     Canvas canvas;
-    float zSpace = 1.513f;
-    float extraJump = 0.106f;
+    float zSpace = 1.514f;
+    float kaytavaVali = 9.0f;
+    float extraJump = 0.108f;
     float hyllyvali = 28.98f;
     int kaytava = 1;
     int hyllyPaikka = 124;
+    int rivi = 0;
+    Keraysera[] keraysera;
 
     private void Awake()
     {
@@ -28,17 +31,45 @@ public class KaytavaManager : MonoBehaviour
     }
     private void Start()
     {
+        keraysera = new Keraysera[] { new Keraysera { kaytava = 1, paikka = 117 }, new Keraysera { kaytava = 2, paikka = 204 }, new Keraysera { kaytava = 2, paikka = 329}, new Keraysera { kaytava = 5, paikka = 118 } };
         timer = GameObject.Find("Timer").GetComponent<TextMeshProUGUI>();
+        player = GameObject.Find("Kerayskone").transform.Find("Rullakko");
+        seuraava = GameObject.Find("Kerayspaikka").GetComponent<TextMeshProUGUI>();
+        etaisyys = GameObject.Find("Etaisyys").GetComponent<TextMeshProUGUI>();
         canvas = GameObject.Find("UI").GetComponent<Canvas>();
         indicator = GameObject.Find("Indicator").transform;
         indicatorOri = indicator.transform.position;
         InitializeNumbers();
         //timer.transform.position = new Vector2(timer.transform.position.x, Screen.height - 70f * canvas.scaleFactor);
-        PlaceIndicator(1, 326);
+        SeuraavaRivi();
     }
+
+    private void SeuraavaRivi()
+    {
+        Keraysera temp = keraysera[rivi];
+        PlaceIndicator(temp.kaytava, temp.paikka);
+        rivi++;
+        if (rivi >= keraysera.Length)
+        {
+            Debug.Log("Poka valmis");
+            rivi = keraysera.Length - 1;
+        }
+    }
+
     private void Update()
     {
         UpdateTime();
+        if (Time.frameCount % 10 == 0)
+            UpdateDistance();
+    }
+
+    private void UpdateDistance()
+    {
+        Vector2 pl = new Vector2(player.position.x, player.position.z);
+        Vector2 dest = new Vector2(indicator.position.x, indicator.position.z);
+        float dist = Vector2.Distance(pl, dest);
+        etaisyys.text = string.Format("{0:0.0}m", dist);
+        if (dist < 2f) SeuraavaRivi();
     }
 
     private void UpdateTime()
@@ -46,7 +77,7 @@ public class KaytavaManager : MonoBehaviour
         time += Time.deltaTime;
         float minutes = Mathf.Floor(time / 60);
         float seconds = Mathf.Floor(time - minutes * 60);
-        float milliseconds = Mathf.Floor((time % 1)* 10f);
+        float milliseconds = Mathf.Floor((time % 1) * 10f);
         timer.text = string.Format("{0:00}:{1:00}.{2:0}", minutes, seconds, milliseconds);
     }
 
@@ -91,7 +122,7 @@ public class KaytavaManager : MonoBehaviour
                     }
                 }
             }
-            else 
+            else
             {
                 for (int it = 0; it < iterations; it++)
                 {
@@ -101,7 +132,7 @@ public class KaytavaManager : MonoBehaviour
                         switch (it)
                         {
                             case 0:
-                                num = 300 + (32- (a * 2 + 2));
+                                num = 300 + (32 - (a * 2 + 2));
                                 break;
                             case 1:
                                 num = 200 + (32 - (a * 2 + 2));
@@ -128,22 +159,52 @@ public class KaytavaManager : MonoBehaviour
 
     void PlaceIndicator(int kaytava, int osoite)
     {
+        seuraava.text = $"0{kaytava}-{osoite}";
         Vector3 pos = indicator.transform.position;
         int hylly = osoite / 100 - 1;
         osoite = osoite % 100;
-        if (osoite % 2 != 0)
+        if (kaytava % 2 != 0)
         {
-            int kerroin = (osoite - 1) / 2;
-            int extra = kerroin / 3;
-            float offset = hylly * hyllyvali + kerroin * zSpace + extra * extraJump;
-            indicator.transform.position = new Vector3(indicatorOri.x, indicatorOri.y, indicatorOri.z + offset);
+            if (osoite % 2 != 0)
+            {
+                int kerroin = (osoite - 1) / 2;
+                int extra = kerroin / 3;
+                float xOffset = (kaytava - 1) * kaytavaVali;
+                float offset = hylly * hyllyvali + kerroin * zSpace + extra * extraJump;
+                indicator.transform.position = new Vector3(indicatorOri.x + xOffset, indicatorOri.y, indicatorOri.z + offset);
+            }
+            else
+            {
+                int kerroin = (osoite - 2) / 2;
+                int extra = kerroin / 3;
+                float xOffset = (kaytava - 1) * kaytavaVali - 3.9f;
+                float offset = hylly * hyllyvali + kerroin * zSpace + extra * extraJump;
+                indicator.transform.position = new Vector3(indicatorOri.x + xOffset, indicatorOri.y, indicatorOri.z + offset);
+            }
         }
         else
         {
-            int kerroin = (osoite - 2) / 2;
-            int extra = kerroin / 3;
-            float offset = hylly * hyllyvali + kerroin * zSpace + extra * extraJump;
-            indicator.transform.position = new Vector3(indicatorOri.x - 3.9f, indicatorOri.y, indicatorOri.z + offset);
+            if (osoite % 2 == 0)
+            {
+                int kerroin = (30 - osoite) / 2;
+                int extra = kerroin / 3;
+                float xOffset = (kaytava - 1) * kaytavaVali;
+                float offset = (2 - hylly) * hyllyvali + kerroin * zSpace + extra * extraJump;
+                indicator.transform.position = new Vector3(indicatorOri.x + xOffset, indicatorOri.y, indicatorOri.z + offset);
+            }
+            else
+            {
+                int kerroin = (29 - osoite) / 2;
+                int extra = kerroin / 3;
+                float xOffset = (kaytava - 1) * kaytavaVali - 3.9f;
+                float offset = (2 - hylly) * hyllyvali + kerroin * zSpace + extra * extraJump;
+                indicator.transform.position = new Vector3(indicatorOri.x + xOffset, indicatorOri.y, indicatorOri.z + offset);
+            }
         }
     }
+}
+public struct Keraysera
+{
+    public int kaytava;
+    public int paikka;
 }
